b1de718d55b00602ec927192d461d627b3aba7c3
furouwang <C112181150@nkust.edu.tw>
2025-10-30 13:01:40 +0800

    Add files via upload

---
+++ ch7-1-1a copy2.php
@@ -0,0 +1,15 @@
+<h1>SID:C1122181150
+   <BR>
+NAME:fu rou </h1>
+<p></p>
+<?php
+define(constant_name:"ANIMALS",value:[
+   "dog",//0
+   "cat",//1
+   "monkey",//2
+   "bird",//3
+]);
+
+echo ANIMALS [1];
+echo "<br>";
+echo ANIMALS [2];
